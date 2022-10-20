eda41a0934ea9a23e621f3b31b554d5a06995dc0
MartinNicolasRusso <russomartin2@gmail.com>
2022-10-20 18:56:38 -0300

    ConnectDataWithApiFinished

--- src/containers/CryptoList/index.tsx
+++ src/containers/CryptoList/index.tsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {FlatList, SafeAreaView} from 'react-native';
 import Crypto from '../../components/Crypto';
-import ApiCryptos from '../../store/Api';
 import {Button} from './styles';
 
 const CryptoList = () => {
+  const [coins, setCoins] = useState([]);
+  const cryptos = async () => {
+    const res = await fetch(
+      'https://data.messari.io/api/v2/assets?fields=id,name,slug,symbol,metrics/market_data',
+    );
+    const data = await res.json();
+    setCoins(data.data);
+  };
+  useEffect(() => {
+    cryptos();
+  }, []);
+
   const renderItem = ({item}) => <Crypto item={item} />;
   return (
     <SafeAreaView>
       <FlatList
-        data={ApiCryptos}
-        keyExtractor={({id}) => id}
+        data={coins}
+        keyExtractor={item => item.id}
         renderItem={renderItem}
       />
       <Button>+ Add Cryptocurrency</Button>
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,19 @@
 export type CryptoTypes = {
-  id: string;
   name: string;
-  slug: string;
-  price: string;
-  market_porcentage: string;
+  symbol: string;
+  price_usd: number;
+  percent_change_usd_last_24_hours: number;
+  id: string;
   image: string;
+  metrics: Metrics;
+  market_data: Market_data;
+};
+
+export type Market_data = {
+  price_usd: number;
+  percent_change_usd_last_24_hours: number;
+};
+
+export type Metrics = {
+  market_data: Market_data;
 };
