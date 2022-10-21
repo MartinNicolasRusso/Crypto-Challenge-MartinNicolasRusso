967c0901a01b2e462e8d2ed0a10a48d61550cff4
MartinNicolasRusso <russomartin2@gmail.com>
2022-10-21 15:34:23 -0300

    FixingENVMistakes

--- src/containers/CryptoList/index.tsx
+++ src/containers/CryptoList/index.tsx
@@ -1,14 +1,15 @@
 import React, {useEffect, useState} from 'react';
 import {FlatList, SafeAreaView} from 'react-native';
 import Crypto from '../../components/Crypto';
-import { CryptoTypes } from '../../types';
+import {CryptoTypes} from '../../types';
 import {Button} from './styles';
+import {BASE_URL} from '@env';
 
 const CryptoList = () => {
   const [coins, setCoins] = useState<CryptoTypes[]>([]);
   const loadCryptosData = async () => {
     const res = await fetch(
-      `https://data.messari.io/api/v2/assets?fields=id,name,slug,symbol,metrics/market_data`,
+      `${BASE_URL}?fields=id,name,slug,symbol,metrics/market_data`,
     );
     const data = await res.json();
     setCoins(data.data);
